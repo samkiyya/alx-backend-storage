d641884a8598fe053edefefa230db5aba871a726
samkiyya <samuelkiyya2012@gmail.com>
2024-10-23 10:18:21 -0700

    Corrected cache key handling

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,32 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+""" Redis Module """
+
+from functools import wraps
 import redis
 import requests
-from functools import wraps
 from typing import Callable
 
-# Create a Redis connection object
-redis_store = redis.Redis()
+redis_ = redis.Redis()
 
-def data_cacher(method: Callable[[str], str]) -> Callable[[str], str]:
-    '''Decorator to cache the output of a method and track access count.'''
+
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url: str) -> str:
-        '''Caches the fetched result and increments the access count.'''
-        
-        # Increment the count for each URL access
-        redis_store.incr(f'count:{url}')
-        
-        # Check if the result is already cached
-        cached_result = redis_store.get(f'result:{url}')
-        if cached_result:
-            # Return the cached result if available
-            return cached_result.decode('utf-8')
-        
-        # Fetch the page content if not cached
-        result = method(url)
-        
-        # Cache the result with a 10-second expiration
-        redis_store.setex(f'result:{url}', 10, result)
-        
-        return result
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
     return wrapper
 
-@data_cacher
+
+@count_requests
 def get_page(url: str) -> str:
-    '''Fetches the HTML content of a URL and caches it.
-    
-    Args:
-        url (str): The URL of the webpage to fetch.
-        
-    Returns:
-        str: The content of the webpage.
-    '''
-    return requests.get(url).text
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
