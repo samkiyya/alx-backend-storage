3237aae0215b1cf7d9450c0047e47c6558769c52
samkiyya <samuelkiyya2012@gmail.com>
2024-10-23 10:40:15 -0700

    Corrected cache key handling

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,38 @@
 #!/usr/bin/env python3
-"""
-Module to provide a cache decorator for web page retrieval.
-"""
-
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
+import requests
+from functools import wraps
 from typing import Callable
 
-redis_client = redis.Redis()
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def cache_decorator(func: Callable) -> Callable:
-    """
-    Decorator to cache the result of a function
-    with a specified expiration time.
-    """
-    def wrapper(*args, **kwargs):
-        """
-        Generate a cache key based on the function name and arguments
-        """
-        cache_key = f"cache:{func.__name__}:{args}"
-
-        cached_result = redis_client.get(cache_key)
-        if cached_result:
-            return cached_result.decode('utf-8')
-
-        result = func(*args, **kwargs)
-
-        redis_client.setex(cache_key, 10, result)
-
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
+    return invoker
 
-    return wrapper
 
-
-@cache_decorator
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    Get the HTML content of a particular URL
-    and cache the result with an expiration time of 10 seconds.
-    """
-    redis_client.incr(f"count:{url}")
-
-    cached_data = redis_client.get(url)
-    if cached_data:
-        return cached_data.decode('utf-8')
-
-    response = requests.get(url)
-    html_content = response.text
-
-    redis_client.setex(url, 10, html_content)
-
-    return html_content
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
