8fdfef1a722b758fa057f47422d07b37075358cc
samkiyya <samuelkiyya2012@gmail.com>
2024-10-23 10:30:44 -0700

    Corrected cache key handling

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,55 @@
 #!/usr/bin/env python3
+"""Module for implementing an expiring web cache and tracker
+"""
 import requests
 import time
 from functools import wraps
-from typing import Dict
 
-cache: Dict[str, str] = {}
+CACHE_EXPIRATION_TIME = 10  # seconds
+CACHE = {}
 
+
+def cache(fn):
+    """_summary_
+
+    Args:
+        fn (function): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    @wraps(fn)
+    def wrapped(*args, **kwargs):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        url = args[0]
+        if url in CACHE and CACHE[url]["timestamp"] + CACHE_EXPIRATION_TIME > \
+                time.time():
+            CACHE[url]["count"] += 1
+            return CACHE[url]["content"]
+        else:
+            content = fn(*args, **kwargs)
+            CACHE[url] = {"content": content,
+                          "timestamp": time.time(), "count": 1}
+            return content
+    return wrapped
+
+
+@cache
 def get_page(url: str) -> str:
-    if url in cache:
-        print(f"Retrieving from cache: {url}")
-        return cache[url]
-    else:
-        print(f"Retrieving from web: {url}")
-        response = requests.get(url)
-        result = response.text
-        cache[url] = result
-        return result
+    """_summary_
 
-def cache_with_expiration(expiration: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            key = f"count:{url}"
-            if key in cache:
-                count, timestamp = cache[key]
-                if time.time() - timestamp > expiration:
-                    result = func(*args, **kwargs)
-                    cache[key] = (count+1, time.time())
-                    return result
-                else:
-                    cache[key] = (count+1, timestamp)
-                    return
-                
+    Args:
+        url (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    global count
+    # increment count
+    count += 1
+    response = requests.get(url)
+    return response.content.decode('utf-8')
